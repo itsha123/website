6ecef54f754ecbefd2e1d7e8ef60b43450a2eb18
Hamid Mossa <itsha123@proton.me>
2025-06-04 00:01:06 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function changeName() {
+    if (document.getElementById("header").innerHTML == "Hi, I'm Hamid Mossa!") {
+    document.getElementById("header").innerHTML = "Hi, I'm itsha123!";
+    } else {
+        document.getElementById("header").innerHTML = "Hi, I'm Hamid Mossa!";
+    }
+}
